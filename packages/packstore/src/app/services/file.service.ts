3fad9ef40b02349c32d6d054bcaadb57d4cf67ac
Marius Schulze <marius.schulze@brainbits.net>
2022-03-08 14:13:42 +0100

    refactor: switch to conf package for storing data from cli

--- packages/packstore/src/app/services/file.service.ts
+++ packages/packstore/src/app/services/file.service.ts
@@ -1,33 +1,30 @@
-import fs from 'fs';
 import { Store } from '../models/store.model';
-import path from 'path';
 import { handleUserNotInitialized } from './input.service';
 import { loginCmd } from '../commands/login.command';
 import { registerCmd } from '../commands/register.command';
+import Conf, { Schema } from 'conf';
 
-const fileDescriptor = 'packstore.json';
-const packagePathDescriptor = 'packstore';
-let configDirPath: string;
-
-export const tryInitConfig = async (): Promise<void> => {
-  configDirPath = getConfigDir();
-  createConfigDirIfNotExists();
-
-  if (!(await configExistsInConfigDir())) {
-    writeConfig();
-  }
+const schema: Schema<Store> = {
+  user: {
+    type: 'object',
+    properties: {
+      id: {
+        type: 'string',
+      },
+      name: {
+        type: 'string',
+      },
+      password: {
+        type: 'string',
+      },
+    },
+  },
 };
 
-export const readConfig = (): Store => {
-  const rawData: string = fs.readFileSync(getConfigPath(), 'utf8');
-  const store: Store = JSON.parse(rawData === '' ? '{}' : rawData);
-
-  return store;
-};
+export const Config = new Conf({ schema });
 
 export const readConfigAndValidate = async (): Promise<Store | never> => {
-  let store: Store = readConfig();
-
+  let store: Store = Config.store;
   if (store.user?.id) {
     return store;
   } else {
@@ -39,46 +36,22 @@ export const readConfigAndValidate = async (): Promise<Store | never> => {
       await registerCmd(undefined);
     }
 
-    store = readConfig();
+    store = Config.store;
 
     return store.user?.id ? store : process.exit();
   }
 };
 
-export const writeConfig = (data?: Store | undefined): void => {
-  fs.writeFileSync(getConfigPath(), JSON.stringify(data ?? {}, null, 2), 'utf8');
+export const writeConfig = (data: Store): void => {
+  if (data) {
+    Config.store = data;
+  } else {
+    console.log('PLEASE DEBUG ME');
+  }
 };
 
 export const isUserConfiguredLocally = async (): Promise<boolean> => {
-  const store: Store = readConfig();
+  const store: Store = Config.store;
 
   return store.user?.id !== undefined && store.user?.name !== undefined && store.user?.password !== undefined;
-};
-
-const configExistsInConfigDir = async (): Promise<boolean> => {
-  return new Promise((r) => fs.access(getConfigPath(), fs.constants.F_OK | fs.constants.W_OK, (e) => r(!e)));
-};
-
-const createConfigDirIfNotExists = (): void => {
-  if (!fs.existsSync(configDirPath)) {
-    fs.mkdirSync(configDirPath, { recursive: true });
-  }
-};
-
-const getConfigDir = (): string => {
-  const platform = process.platform;
-  const homeDir = process.env[platform === 'win32' ? 'USERPROFILE' : 'HOME'];
-  let configDir;
-
-  if (platform === 'win32') {
-    configDir = path.join(homeDir!, 'AppData/Roaming', packagePathDescriptor);
-  } else {
-    configDir = path.join(homeDir!, '.', packagePathDescriptor);
-  }
-
-  return configDir;
-};
-
-const getConfigPath = (): string => {
-  return `${configDirPath}/${fileDescriptor}`;
 };
