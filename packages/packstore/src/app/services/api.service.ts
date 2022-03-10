84ba2506dacbe60fca8ea59451c0621aee3f726f
Marius Schulze <marius.schulze@brainbits.net>
2022-03-10 21:07:10 +0100

    feat(packstore): include headers

--- packages/packstore/src/app/services/api.service.ts
+++ packages/packstore/src/app/services/api.service.ts
@@ -1,13 +1,25 @@
-import fetch from 'node-fetch';
+import fetch, { Headers } from 'node-fetch';
 import { handleInvalidApiStatus } from './input.service';
 import { User } from '../models/user.model';
+import { Config } from './file.service';
+
+const createHeaders = (withAuth = true): Headers => {
+  const headers: Headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+
+  if (withAuth) {
+    headers.append('Auth', JSON.stringify(Config.get('user')));
+  }
+
+  return headers;
+};
 
 export const createUser = async (userName: string, password: string): Promise<User> => {
   try {
     const response = await fetch('http://localhost:7000/api/auth/register', {
       method: 'post',
       body: JSON.stringify({ name: userName, password: password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: createHeaders(false),
     });
 
     if (!response.ok) {
@@ -27,7 +39,7 @@ export const loginUser = async (userName: string, password: string): Promise<Use
     const response = await fetch('http://localhost:7000/api/auth/login', {
       method: 'post',
       body: JSON.stringify({ name: userName, password: password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: createHeaders(false),
     });
 
     if (!response.ok) {
@@ -47,7 +59,7 @@ export const addPackage = async (userId: string, packageName: string): Promise<b
     const response = await fetch('http://localhost:7000/api/store', {
       method: 'post',
       body: JSON.stringify({ userId, packageName }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: createHeaders(),
     });
 
     if (!response.ok) {
@@ -63,29 +75,29 @@ export const addPackage = async (userId: string, packageName: string): Promise<b
 };
 
 export const removePackage = async (userId: string, packageName: string): Promise<boolean> => {
-    try {
-      const response = await fetch('http://localhost:7000/api/store', {
-        method: 'delete',
-        body: JSON.stringify({ userId, packageName }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (!response.ok) {
-        return Promise.reject(await handleInvalidApiStatus(response));
-      }
-  
-      const data = (await response.json()) as boolean;
-  
-      return data;
-    } catch (error) {
-      return Promise.reject(error);
+  try {
+    const response = await fetch('http://localhost:7000/api/store', {
+      method: 'delete',
+      body: JSON.stringify({ userId, packageName }),
+      headers: createHeaders(),
+    });
+
+    if (!response.ok) {
+      return Promise.reject(await handleInvalidApiStatus(response));
     }
-  };
+
+    const data = (await response.json()) as boolean;
+
+    return data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 export const listPackages = async (userId: string): Promise<string[]> => {
   try {
     const url = `http://localhost:7000/api/store?${new URLSearchParams({ userId })}`;
-    const response = await fetch(url);
+    const response = await fetch(url, { method: 'get', headers: createHeaders() });
 
     if (!response.ok) {
       return Promise.reject(await handleInvalidApiStatus(response));
